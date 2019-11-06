af687b3ea0b37a70be8975e9e7f3d96e6881537c
Matthew-E-Gould <49723415+Matthew-E-Gould@users.noreply.github.com>
2019-11-06 12:15:27 +0000

    added vue.js implementation

--- resources/views/reviews/show.blade.php
+++ resources/views/reviews/show.blade.php
@@ -2,21 +2,37 @@
 
 @section('content')
 
-<div class="container-fluid row">
+<div class="container-fluid row" id="app">
+
   <div class="card col-lg-3 col-md-12">
     <img src="{{asset('storage/'.strval($review->game->img_name))}}" class="card-img-top" alt="...">
     <div class="card-body">
-      <p class="card-text">{{ $review->game->title }}</p>
+      <p class="card-text">@{{ review.game.title }}</p>
     </div>
   </div>
 
   <div class="card col-lg-9 col-md-12">
     <div class="card-header">
-      {{ $review->heading }}- {{ $review->user->name }}
+      @{{ review.heading }} by {{ $review->user->name }}
+  <!-- I understand that {{$review->user->name}} should be @{{review.user.name}} but it wont work :( -->
     </div>
     <div class="card-body">
-      <p class="card-text">{{ $review->content }}</p>
+      <p class="card-text">@{{ review.content }}</p>
     </div>
   </div>
 </div>
+@endsection
+
+
+@section('vue-js')
+	<script>
+		var app = new Vue({
+	  	el: '#app',
+	  	data: {
+	    	review: {!! $review !!}
+	  	},
+			computed: {},
+			methods: {}
+		})
+	</script>
 @endsection
